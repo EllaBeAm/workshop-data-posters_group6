4d76677792c6dfa1b379f5920cc25f57009ceea7
EllaBeAm <ellabenamram@gmail.com>
2022-02-17 16:20:55 +0100

    faces quotes2

--- src/main/kotlin/examples/framework/ImageTreatment001_copy2.kt
+++ src/main/kotlin/examples/framework/ImageTreatment001_copy2.kt
@@ -3,16 +3,22 @@ package examples.framework
 import FilmGrain
 import archives.LoadedArticle
 import archives.localArchive
+import org.openrndr.animatable.Animatable
+import org.openrndr.animatable.easing.Easing
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.color.rgb
+import org.openrndr.draw.loadFont
+import org.openrndr.draw.tint
 import org.openrndr.events.Event
+import org.openrndr.extra.color.spaces.toOKHSVa
 import org.openrndr.extra.compositor.*
-import org.openrndr.extra.fx.blend.Add
-import org.openrndr.extra.fx.blur.HashBlur
+import org.openrndr.extra.fx.blend.Multiply
+import org.openrndr.extra.fx.blur.ApproximateGaussianBlur
 import org.openrndr.extra.fx.color.Duotone
 import org.openrndr.extra.fx.color.LumaOpacity
-import org.openrndr.extra.fx.distort.Perturb
 import org.openrndr.extra.fx.distort.StackRepeat
+import org.openrndr.extra.fx.patterns.Checkers
 import org.openrndr.extra.fx.shadow.DropShadow
 import org.openrndr.extra.gui.GUI
 import org.openrndr.extra.gui.addTo
@@ -20,9 +26,11 @@ import org.openrndr.extra.noise.uniform
 import org.openrndr.extra.parameters.ActionParameter
 import org.openrndr.extra.parameters.Description
 import org.openrndr.extras.imageFit.imageFit
+import org.openrndr.shape.Rectangle
+import org.openrndr.writer
+import tools.statistics
 
 // Image treatment demonstration. Learn from it, don't just copy ;)
-// Here we learn that we are not limited to using a single layer
 
 fun main() = application {
     configure {
@@ -46,63 +54,45 @@ fun main() = application {
 
         // all our image treatment happens inside a compose block
         val composite = compose {
+            layer {
 
-            // our first image layer, we can name them to make it a bit easier to find it back
-            var imageLayer0 = layer {
                 // here we create variables that we will use to randomize the settings of StackRepeat
                 var xo = 0.0
                 var yo = 0.0
+                var xx = listOf(40.00, 340.00)
+
+
 
                 // listen for a new article event and randomize
                 onNewArticle.listen {
                     xo = Double.uniform(-0.25, 0.25)
                     yo = Double.uniform(-0.25, 0.25)
-                }
+
+        }
 
                 draw {
                     // draw the article image full page but have 10 px margins
-                    drawer.imageFit(article.images[0], drawer.bounds.offsetEdges(-10.0))
+                    drawer.imageFit(article.images[0], xx, 40.00)
                 }
-                // add a drop shadow effect first
-                post(DropShadow())
-                post(StackRepeat()) {
-                    // we can learn which settings StackRepeat has by moving the cursor over StackRepeat and
-                    // pressing command-b on macOS or ctrl-b on Windows/Linux
-                    this.xOffset = xo
-                    this.yOffset = yo
-                }.addTo(gui)
+
+
+
 
                 // Add a duotone effect, use the default colors
                 post(Duotone()) {
-                     this.backgroundColor = ColorRGBa.BLACK
-                     this.foregroundColor = ColorRGBa.WHITE
+                    this.backgroundColor = ColorRGBa.BLACK
+                    this.foregroundColor = ColorRGBa.WHITE
                 }
+
 
             }
-
-            var imageLayer1 = layer {
-
-                var perturbPhase = 0.0
-                onNewArticle.listen {
-                    perturbPhase = Double.uniform(-1.0, 1.0)
-                }
-
-                draw {
-                    drawer.imageFit(article.images[0], drawer.bounds.offsetEdges(-100.0))
-                }
-
-            }
-
         }
         onNewArticle.trigger(article)
 
         gui.add(settings)
         extend(gui)
         extend {
-            gui.visible = mouse.position.x < 200.0
-
             composite.draw(drawer)
-
         }
     }
 }
