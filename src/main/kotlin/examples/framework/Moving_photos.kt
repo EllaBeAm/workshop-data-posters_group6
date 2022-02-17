82519434fe36b2085b71143fe16468b4e79e07fd
EllaBeAm <ellabenamram@gmail.com>
2022-02-17 16:59:25 +0100

    moving photos

--- src/main/kotlin/examples/framework/Moving_photos.kt
+++ src/main/kotlin/examples/framework/Moving_photos.kt
@@ -1,34 +1,17 @@
 package examples.framework
 
-import FilmGrain
 import archives.LoadedArticle
 import archives.localArchive
-import org.openrndr.animatable.Animatable
-import org.openrndr.animatable.easing.Easing
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.color.rgb
-import org.openrndr.draw.loadFont
-import org.openrndr.draw.tint
 import org.openrndr.events.Event
-import org.openrndr.extra.color.spaces.toOKHSVa
 import org.openrndr.extra.compositor.*
-import org.openrndr.extra.fx.blend.Multiply
-import org.openrndr.extra.fx.blur.ApproximateGaussianBlur
 import org.openrndr.extra.fx.color.Duotone
-import org.openrndr.extra.fx.color.LumaOpacity
-import org.openrndr.extra.fx.distort.StackRepeat
-import org.openrndr.extra.fx.patterns.Checkers
-import org.openrndr.extra.fx.shadow.DropShadow
 import org.openrndr.extra.gui.GUI
-import org.openrndr.extra.gui.addTo
 import org.openrndr.extra.noise.uniform
 import org.openrndr.extra.parameters.ActionParameter
 import org.openrndr.extra.parameters.Description
 import org.openrndr.extras.imageFit.imageFit
-import org.openrndr.shape.Rectangle
-import org.openrndr.writer
-import tools.statistics
 
 // Image treatment demonstration. Learn from it, don't just copy ;)
 
